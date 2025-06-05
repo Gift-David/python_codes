b91b49ea803465d5b5b55cf35b2117c7ba3a03fb
David Gift <giftdavid189@gmail.com>
2025-06-05 13:21:24 +0100

    edited the inventory management file

--- simpleInventory/inventory_system.py
+++ simpleInventory/inventory_system.py
@@ -12,25 +12,37 @@ def display_menu():
 
 inventory = [] # list to store all products
 
-def view_products():
+
+# Recheck that this functions breaks the calling function on true
+
+# validate_nonempty_inventory()
+def validate_nonempty_inventory():
     # returns a msg if inventory list is empty
     if not inventory:
         print("Inventory is empty")
-        return
+    return None
+
+
+def view_products():
     
+    # returns a msg if inventory list is empty
+    if not inventory:
+        print("Inventory is empty")
+
+        return False
     print("---Current Inventory--")
-    print(f"{"id": < 8} | {"name": < 25} | {"quantity": < 10} | {"price":< 10.2}")
+    print(f"{"id":<8} | {"name":<25} | {"quantity":<10} | {"price":<10} ($)")
     print("-" * 60)
 
     for product in inventory:
-        print(f"{product["id"]: < 8} | {product["name"]: < 25} | {product["quantity"]: < 10} | {product["price"]:< 10.2f}")
+        print(f"{product["id"]:<8} | {product["name"]:<25} | {product["quantity"]:<10} | ${product["price"]:<10.2f}")
 
 
 def add_product(product_id, product_name, product_quantity, product_price):
     
     for product in inventory:
-        # validates product_id don't exist
-        if product["id"].lower == product_id:
+        # checks if product_id exist
+        if product.get("id").lower() == product_id.lower():
             print(f"Error: Product with ID:{product_id} already exists. Enter a unique ID")
             return False
     # validates the product quantity and price is a non negative integer
@@ -54,17 +66,54 @@ def add_product(product_id, product_name, product_quantity, product_price):
     print(f"Product '{product_name}' with ID:{product_id} entered successfully!")
     return True
 
-def update_product(product_id, product_quantity):
-    
+# function to validate product_ID
+def validate_id(product_id):
     for product in inventory:
-        # validates product id exists
-        if not product["id"].lower == product_id:
+    # checks if product id doesn't exists
+        if not product.get("id").lower() == product_id.lower():
             print(f"Error: Product with ID:{product_id} does not exists. Enter an existing ID")
-            return False
+            return True
+
+def update_product(product_id, product_quantity):
+    validate_id(product_id)
     # validates the product quantity and price is a non negative integer
     if product_quantity < 0:
         print(f"Error: The quantity can't be negative")
         return False
+    
+    for product in inventory:
+        if product.get("id").lower() == product_id.lower():
+            product["quantity"] = product_quantity
+            print(f"Product {product["name"]} quantity has been updated successfully")
+            return True
+
+def search_product(product_id):
+    validate_id(product_id)
+    for product in inventory:
+        if product["id"] == product_id:
+            print(f"{"id":<8} | {"name":<25} | {"quantity":<10} | {"price":<10} ($)")
+            print("-" * 60)
+            print(f"{product["id"]:<8} | {product["name"]:<25} | {product["quantity"]:<10} | ${product["price"]:<10.2f}")
+            return True
+    
+def remove_product(product_id):
+    validate_id(product_id)
+    # confirms the deleting
+    for product in inventory:
+        if product.get("id").lower() == product_id.lower():
+            print(f"{product["id"]:<8} | {product["name"]:<25} | {product["quantity"]:<10} | {product["price"]:<10.2f}")
+            confirm_msg = input("You're about to delete this product. Confirm (yes/no)").lower().strip()
+            if confirm_msg == "no":
+                break
+            elif confirm_msg == "yes":
+                inventory.remove(product)
+                print(f"Product removed successfully")
+            else:
+                print(f"Invalid input.")
+                break
+
+    return True
+            
     
         
 
@@ -74,25 +123,55 @@ def update_product(product_id, product_quantity):
 def main():
     while True:
         display_menu()
-        choice = input(int("Enter your choice: "))
+        choice = int(input(("Enter your choice: ")))
 
         if choice == 1:
             # call function display all products
+            view_products()
             pass
         elif choice == 2:
             # Promt for and call function to add a product
+            print("---Add a new product")
+            product_id = str(input("Enter the product ID: ")).strip()
+            product_name = str(input("Enter the product's name: ")).strip()
+            product_quantity = int(input("Enter the quantity in stock: "))
+            product_price = float(input("Enter the unit price: "))
+
+            add_product(product_id, product_name, product_quantity, product_price)
             pass
         elif choice == 3:
             # Prompt for and call function to update a product quantity with its id
+            # validate_nonempty_inventory()
+            if not inventory:
+                print("Inventory is empty")
+
+                return True
+            product_id = str(input("Enter the product ID: ")).strip()
+            product_quantity = int(input("Enter the quantity in stock: "))
+            update_product(product_id, product_quantity)
             pass
         elif choice == 4:
             # Prompt for id and call function to get the product
+            # validate_nonempty_inventory()
+            product_id = str(input("Enter the product ID: ")).strip()
+            search_product(product_id)
             pass
         elif choice == 5:
             # Prompt for and call function to remove a product
+            # validate_nonempty_inventory()
+            # if not inventory:
+            #     print("Inventory is empty")
+
+            #     return True
+
+            product_id = str(input("Enter the product ID: ")).strip()
+            remove_product(product_id)
             pass
         elif choice == 6:
             print("Good bye!")
             break
         else:
             print("Invalid choice. Please try again.")
+
+# if __name__ == "___main___":
+main()
